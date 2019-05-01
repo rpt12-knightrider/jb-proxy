4639fc7355b5562b244825bf537ee31314646f6b
Jesse Bell <jkurtasbell@gmail.com>
2019-04-30 21:38:58 -0700

    Run Description and Author-Info Services on Proxy

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var path = require('path');
+var cors = require('cors');
 
 var app = express();
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.urlencoded( {extended: false }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('hi, there');
