487512849a1d98f3cb02a38e7f2e73b091ec01aa
Jesse Bell <jkurtasbell@gmail.com>
2019-06-21 13:05:05 -0400

    Comment Out script Tags on index.html

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ var app = express();
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, './public')));
+app.use('/:id', express.static(`${__dirname}/public`));
 app.use(express.urlencoded( {extended: false }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
@@ -18,7 +19,7 @@ app.get('/', (req, res) => {
   res.send('hi, there');
 });
 
-const port = 3000;
+const port = 3006;
 
 app.listen(port, () => {
   console.log(`listening at ${port}`);
